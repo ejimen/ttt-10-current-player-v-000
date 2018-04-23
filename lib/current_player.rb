b5f4a5a48baa8c895cacb4f8e3e5bb670eec15a1
Elizabeth Jimenez <lizliz123@gmail.com>
2018-04-23 18:39:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,26 @@
-def turn_count(board)
-  counter = 0
-  board.each do |space|
-    if space == "X" || space == "0"
-      counter += 1
-    end
-end
-counter
-end
-
+def turn_count(turn)
+  #counter = 1
+  move_count = []
+  turn.each do |unit|
+    #counter = 0
+    if unit == "X" || unit == "O"
+      #puts "spot taken"
+      move_count.push(1)
+    #else 
+      #puts "spot free"
+      #counter += 1
+    end 
+  end 
+  move_count.length.to_i
+end 
+ 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "0"
-end
+  #if turn_count(board) %  == 0
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O" 
+    #puts "divisible by 2"
+    end 
+end 
