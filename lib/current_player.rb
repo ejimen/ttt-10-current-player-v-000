d1c92f6e4014a0cdcd5c38c959727aebd0949e68
Elizabeth Jimenez <lizliz123@gmail.com>
2018-04-23 18:37:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
-  board.each do |board|
-    if board == "X" || board == "0"
+  board.each do |space|
+    if space == "X" || space == "0"
       counter += 1
     end
 end
