a09f8484b4832bb926e92aeeccca0472a1ac0dea
Elizabeth Jimenez <lizliz123@gmail.com>
2018-04-23 18:36:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
-
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "0"
+def turn_count(board)
+  counter = 0
+  board.each do |board|
+    if board == "X" || board == "0"
+      counter += 1
+    end
 end
+counter
+end
+
